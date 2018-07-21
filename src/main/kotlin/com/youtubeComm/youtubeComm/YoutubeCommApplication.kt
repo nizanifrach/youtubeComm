5c827e96430171c1ff326bd8fed144b5b2882617
nizanifrach <nizanifrach@gmail.com>
2018-07-21 16:04:16 +0300

    Add files via upload

---
+++ src/main/kotlin/com/youtubeComm/youtubeComm/YoutubeCommApplication.kt
@@ -0,0 +1,11 @@
+package com.youtubeComm.youtubeComm
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class YoutubeCommApplication
+
+fun main(args: Array<String>) {
+    runApplication<YoutubeCommApplication>(*args)
+}
---
+++ src/test/kotlin/com/youtubeComm/youtubeComm/YoutubeCommApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.youtubeComm.youtubeComm
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class YoutubeCommApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
